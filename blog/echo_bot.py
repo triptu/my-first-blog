4d933e401436e17042e0c53474c2072f49b2c2ac
Tushar <tushutripathi@gmail.com>
2016-08-15 22:19:46 +0530

    Made it echo_bot

--- blog/echo_bot.py
+++ blog/echo_bot.py
@@ -11,10 +11,15 @@ def get(command):
 
 def echo(msg):
     data = json.loads(msg)
-    with open('new.txt', 'a') as f:
+    with open('new.txt', 'w') as f:
         f.write(msg)
         f.write('\n---------------------------------------------------------\n\n\n')
-    user_id = data['result'][0]['message']['from']['id']
-    reply = "This is my reply."
+    user_id = data['message']['from']['id']
+    f_name = data['message']['from']['first_name']
+    user_msg = data['message']['text']
+    with open('new.txt', 'a') as f:
+        f.write('User name-%s\n' %f_name)
+        f.write('message- %s' %user_msg)
+    reply = user_msg
     send_msg = get('sendmessage?text='+reply+'&chat_id='+str(user_id))
 
